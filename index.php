ec1cb3e0b59b3312e3782cfceb18dfdd8d3d0dc8
proveedor532 <proveedor532@culqi.com>
2024-11-26 23:41:20 -0500

    login y menu v2

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <title>Home - MyLinkedIn</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/index.css">
-
 </head>
 <body>
 <?php 
