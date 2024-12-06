ff059c956d9bae802ce5c11901106a350b6a6e9c
Renzo <guns212000@gmail.com>
2024-12-05 23:25:34 -0500

    prueba a

--- sections/notificaciones.php
+++ sections/notificaciones.php
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['mensaje_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notificaciones</title>
-    <link rel="stylesheet" href="../css/notificaciones_postulante.css">
+    <link rel="stylesheet" href="css/notificaciones_postulante.css">
    
 </head>
 <body>
