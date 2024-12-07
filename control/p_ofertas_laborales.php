51033f93915322bfb23a053f8552c3eca97da2df
YEFRI <yeffersonquinonez568@gmail.com>
2024-12-06 19:32:36 -0500

    paginacionofertas

--- control/p_ofertas_laborales.php
+++ control/p_ofertas_laborales.php
@@ -1,5 +1,16 @@
 <?php
 include("sections/conexion.php");
+
+
+$registros_por_pagina = 5;
+
+// Determinar desde qué registro empezar
+if (isset($_GET["valor"])) {
+    $limite_inicio = intval($_GET["valor"]);
+} else {
+    $limite_inicio = 0;
+}
+
 $sql = "
 SELECT 
     e.id_empresa,
@@ -15,10 +26,31 @@ INNER JOIN
 INNER JOIN 
     estado_propuesta ep ON p.id_estado_propuesta = ep.id_estado_propuesta
 WHERE
-    ep.nombre_estado_propuesta IN ('activo','expirado');
+    ep.nombre_estado_propuesta IN ('activo','expirado')
 
+LIMIT $limite_inicio, $registros_por_pagina;
 ";
 
+
 $fila = mysqli_query($cn, $sql);
+
+if (!$fila) {
+    die("Error en la consulta SQL: " . mysqli_error($cn));
+}
+
+
+
+$sql_total = "
+SELECT COUNT(*) AS total 
+FROM detalle_empresa_propuesta dep
+INNER JOIN propuesta p ON dep.id_propuesta = p.id_propuesta
+INNER JOIN estado_propuesta ep ON p.id_estado_propuesta = ep.id_estado_propuesta
+WHERE ep.nombre_estado_propuesta IN ('activo', 'expirado');
+";
+$resultado_total = mysqli_query($cn, $sql_total);
+$total_registros = mysqli_fetch_assoc($resultado_total)['total'];
+
+
+$total_paginas = ceil($total_registros / $registros_por_pagina);
 
 ?>
